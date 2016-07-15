4967bcf61ce1612845fdf602a2606116aa95936c
Ken McGrady <ken.mcgrady@gmail.com>
2016-07-15 10:06:57 -0700

    Initial Scaffold for application

---
+++ knexfile.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/pokegogo_dev'
+  }
+};
---
+++ server.js
@@ -0,0 +1,55 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ silent: true });
+}
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+
+const app = express();
+
+app.disable('x-powered-by');
+
+if (process.env.NODE_ENV !== 'test') {
+  const morgan = require('morgan');
+
+  app.use(morgan('short'));
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cookieSession({
+  keys: [
+    process.env.SESSION_KEY1,
+    process.env.SESSION_KEY2
+  ]
+}));
+
+app.use(express.static(path.join('public')));
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+// eslint-disable-next-line max-params
+app.use((err, _req, res, _next) => {
+  // eslint-disable-next-line no-console
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
+app.listen(port, () => {
+  if (process.env.NODE_ENV !== 'test') {
+    // eslint-disable-next-line no-console
+    console.log('Listening on port', port);
+  }
+});
+
+module.exports = app;
